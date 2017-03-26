b24425a5e2fe9540a98cec4b3518c3450f568fcb
bqbs <codemonkeyjc@gmail.com>
2017-03-27 00:30:37 +0800

    修改文件

--- power.py
+++ power.py
@@ -3,6 +3,7 @@
 __author__ = 'bat'
 
 import itchat
+from itchat.content import *
 
 
 @itchat.msg_register(itchat.content.TEXT)
@@ -10,8 +11,23 @@ def text_reply(msg):
     pass
 
 
+@itchat.msg_register(TEXT, isGroupChat=True)
+def text_reply_group(msg):
+    if msg['isAt']:
+        itchat.send(u'@%s\u2005I received: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName'])
+
+
+@itchat.msg_register(TEXT)
+def text_reply(msg):
+    print msg
+    if msg['ToUserName'] == u'filehelper':
+        print '\n######################true'
+        itchat.send('%s: %s' % (msg['Type'], msg['Text']))
+
+
 def main():
     itchat.auto_login()
+    itchat.run()
 
 
 if __name__ == '__main__':
