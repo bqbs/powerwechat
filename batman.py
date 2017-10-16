c026ca3241d38c1a7d4217c01fb73081bb6b59b7
bqbs <codemonkeyjc@gmail.com>
2017-10-16 23:27:26 +0800

    修改文件

--- batman.py
+++ batman.py
@@ -5,10 +5,11 @@ __author__ = 'Lian'
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-#GPIO.setwarnings(False)
+# GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 GPIO.output(18, True)
 GPIO.output(18, False)
+
 
 def calling_batman():
     print("I am batman")
